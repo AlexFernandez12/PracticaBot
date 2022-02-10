b4a243435dd46ddc4baf8ee676e84c0445992b85
Alex <afd0001@alu.medac.es>
2022-02-10 16:22:09 +0100

    eeeeeeeeeeeeeeeeeeeeeeee

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $token = '5190510451:AAEB_CmkxY-VXdoB8Fkwznrb3SVb_8YKhHc';
 $website = 'https://api.telegram.org/bot'.$token;
  
+$urldom = "messi.jpg";
 $input = file_get_contents('php://input');
 $update = json_decode($input, TRUE);
  
