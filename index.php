422afe609c45f2e2cff345fd72866e9102a414a6
Alex <afd0001@alu.medac.es>
2022-02-14 17:50:38 +0100

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- index.php
+++ index.php
@@ -80,11 +80,11 @@ function redes(){
 
 
 function sendMessage($chatId, $response, $keyboard = NULL) {
-    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
-    file_get_contents($url);
     if (isset($keyboard)) {
         $teclado = '&reply_markup={"keyboard":['.$keyboard.'], "resize_keyboard":true, "one_time_keyboard":true}';
     }
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response).$teclado;
+    file_get_contents($url);
 }
  
 function getNoticias($chatId){
