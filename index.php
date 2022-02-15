5e8654f504573be07922739a1f144917fa3eddde
Alex <afd0001@alu.medac.es>
2022-02-15 15:38:12 +0100

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeEeeeeeeEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ switch($message) {
 
 
 function sendMessage($chatId, $response, $keyboard = '') {
-    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response).$teclado;
     file_get_contents($url);
     if (isset($keyboard)) {
         $teclado = '&reply_markup={"keyboard":['.$keyboard.'], "resize_keyboard":true, "one_time_keyboard":true}';
