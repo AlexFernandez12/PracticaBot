49380469484539cafc6a9d8a176b6580b08d816a
Alex <afd0001@alu.medac.es>
2022-02-17 16:18:29 +0100

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeEeeeeeeEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeEEEeeeeEeeeeEeeeeeeeeeeeeeeeeeeee

--- index.php
+++ index.php
@@ -73,12 +73,9 @@ switch($message) {
     
 
 
-function sendMessage($chatId, $response, $keyboard=NULL) {
-    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response).$teclado;
+function sendMessage($chatId, $response) {
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
-    if (isset($keyboard)) {
-        $teclado = '&reply_markup={"keyboard":['.$keyboard.'], "resize_keyboard":true, "one_time_keyboard":true}';
-    }
 
 }
 /*
