b51b7302cf02e7d1a3285c9469cb5beac6362dce
Alex <afd0001@alu.medac.es>
2022-02-21 17:18:06 +0100

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeEeeeeeeEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeEEEeeeeEeeeeEeeeeeeeeeeeeeeeeeeeeeeeeeeE

--- index.php
+++ index.php
@@ -57,13 +57,13 @@ switch($message) {
             case '/periodico':
                 $keyboard = array('keyboard' =>
                 array(array(
-                    array('text'=>'https://as.com/','callback_data'=>"1"),
+                    array('text'=>'AS','callback_data'=>"1"),
                 ),
                 array(
-                    array('text'=>'https://www.mundodeportivo.com/','callback_data'=>"2"),
+                    array('text'=>'MundoDeportivo','callback_data'=>"2"),
                 ),
                     array(
-                        array('text'=>'https://www.marca.com/','callback_data'=>"3")
+                        array('text'=>'MARCA','callback_data'=>"3")
                     )), 'one_time_keyboard' => false, 'resize_keyboard' => true
             );
             file_get_contents('https://api.telegram.org/bot5190510451:AAEB_CmkxY-VXdoB8Fkwznrb3SVb_8YKhHc/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($keyboard).'&text=Escoja un periodico por teclado');
@@ -87,9 +87,18 @@ switch($message) {
             $teclado = ["https://www.youtube.com/"],["Pos Ok"],["Pos Ok"];
             sendMessage($chatId, $response, $teclado);
             break;*/
-            case 'as':
+            case 'MundoDeportivo':
                 $response  = 'https://www.mundodeportivo.com/';
-                sendMessage($chatId, $response, TRUE);
+                sendMessage($chatId, $response, FALSE);
+                break;
+                case 'AS':
+                    $response  = 'https://as.com/';
+                    sendMessage($chatId, $response, FALSE);
+                    break;
+                    case 'MARCA':
+                        $response  = 'https://www.marca.com/';
+                        sendMessage($chatId, $response, FALSE);
+                        break;
         case 'futbol':
             getNoticias($chatId, 1);
             break;
